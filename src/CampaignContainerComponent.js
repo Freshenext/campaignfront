2187f84ada0b94a94d6d772d2a43e7dbd5a0fa63
Francis Rodriguez <francisucsd@outlook.com>
2021-09-07 22:21:12 -0400

    Added more CSS

--- src/CampaignContainerComponent.js
+++ src/CampaignContainerComponent.js
@@ -1,13 +1,35 @@
-import {Container, Grid} from "@material-ui/core";
+import {Button, Container, Grid} from "@material-ui/core";
+import useStore from "./store";
+import {useEffect} from "react";
 
 export default function CampaignContainerComponent(){
-    return <Container>
-        <Grid container>
-            <Grid xs={3} item>Campaign #1</Grid>
-            <Grid xs={3} item>Campaign #2</Grid>
-            <Grid xs={3} item>Campaign #3</Grid>
-            <Grid xs={3} item>Campaign #4</Grid>
-            <Grid xs={3} item>Campaign #5</Grid>
+    const { campaigns, fetchCampaigns } = useStore(state => state);
+
+    useEffect(() => {
+        fetchCampaigns();
+    }, []);
+
+    return <Grid container>
+        {campaigns.map(campaign => {
+            return <Grid xs={4} className="gridFlexed">
+                <div className="divCampaign">
+                    <img src={campaign.urlFull} className="imgCampaigns" />
+                    <span>{campaign.name}</span>
+                </div>
+                <div className="divHiddenCampaign divHiddenOpacity">
+                    123
+                </div>
+                <div className="divHiddenCampaign divHiddenCard">
+                    <Button
+                        variant="contained"
+                        className="primaryBackgroundColor"
+                        onClick={_ => window.open(campaign.url, '_blank')}
+                    >Click here to go</Button>
+                </div>
+            </Grid>
+        })}
+        <Grid xs={3} item>
+            <img src="" />
         </Grid>
-    </Container>
+    </Grid>
 }
