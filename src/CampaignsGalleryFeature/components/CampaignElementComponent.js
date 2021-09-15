42434581f4c055e5911745d8b71e1b58cd2301e1
Francis Rodriguez <francisucsd@outlook.com>
2021-09-14 23:43:57 -0400

    Added on mobile, show click demo.

--- src/CampaignsGalleryFeature/components/CampaignElementComponent.js
+++ src/CampaignsGalleryFeature/components/CampaignElementComponent.js
@@ -7,9 +7,11 @@ export default function CampaignElementComponent({urlFull, name, url, isMobile, 
     const [isHovered, setIsHovered] = useState(false);
     const classes = ['gridFlexed', isMobile === false ? "hideOnMobile" : '', isDesktop === false ? "hideOnDesktop" : ""]
     const theme = useTheme();
+    const matches = useMediaQuery(theme.breakpoints.down("xs"));
+    const [mobileDiv, setMobileDiv] = useState(false);
     useEffect(() => {
-        console.log(theme.breakpoints);
-    }, [theme]);
+        setMobileDiv(matches);
+    }, [matches]);
     return (
         <Grid xs={6} sm={4} md={3} lg={3} className={classes.join(' ')}
               onMouseEnter={() => setIsHovered(true)}
@@ -28,6 +30,7 @@ export default function CampaignElementComponent({urlFull, name, url, isMobile, 
             {isHovered && <div className={"divHiddenCampaign divHiddenOpacity hideOnMobile"}>
             </div>
             }
+
             {isHovered && <div className="divHiddenCampaign divHiddenCard hideOnMobile">
                 <Button
                     variant="contained"
@@ -37,6 +40,15 @@ export default function CampaignElementComponent({urlFull, name, url, isMobile, 
 
                 >View Demo</Button>
             </div>}
+            {mobileDiv && <Box textAlign="center" style={{ marginTop: '0 !important'}}>
+                <Button
+                    variant="contained"
+                    className="primaryBackgroundColor"
+                    onClick={_ => window.open(url, '_blank')}
+                    style={{ textTransform: "unset", borderRadius: '10px'}}
+
+                >View Demo</Button>
+            </Box>}
 
         </Grid>
     )
